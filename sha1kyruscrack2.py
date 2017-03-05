2ddbb8bb308467bf7248518c2b4baacc66fb3bc7
Chris Castro <cmc-castro@hotmail.com>
2017-03-05 16:08:34 -0500

    Update sha1kyruscrack2.py

--- sha1kyruscrack2.py
+++ sha1kyruscrack2.py
@@ -2,6 +2,7 @@ import hashlib
 
 
 answer = ""
+# Alphabet for testing each letter against puzzle
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", ".", "-", "?", "!", ":", ";", "(", ")", "[", "]", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "@", "#", "$", "%", "^", "&", "*", "+", "~", "`", "\\", "/", "<", ">", "{", "}", "_", "|", "\n"]
 
 cnt = 1
@@ -9,10 +10,12 @@ cnt = 1
 def hashmaker(word):
 	return hashlib.sha1(word).hexdigest() + "\n"
 
+# Grabs line from puzzle
 with open("part2.txt","r") as f:
     for line in f:
     	print cnt
     	cnt+=1
+	# Compares generated hash for each alphabet letter to the hash in the puzzle
         for letter in alphabet:
         	print "Comparing... %r" % line 
         	if line == hashmaker(answer + letter):
@@ -20,6 +23,7 @@ with open("part2.txt","r") as f:
         		print "Y"
         		answer = answer + letter
         		break
+		# Compares uppercase letters
         	elif line == hashmaker(answer + letter.upper()):
         		#print "second"
         		print "Y"
@@ -31,7 +35,7 @@ with open("part2.txt","r") as f:
 
 print "The answer is %r" % answer
 
-
+# Writes answer to text file
 print "Writing to part3.txt..."
 
 shafile2 = open("test.txt", "w")
