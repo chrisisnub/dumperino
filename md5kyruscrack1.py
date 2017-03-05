e3ee39addf183b70cb805ed8dccb2b48ee226aa1
Chris Castro <cmc-castro@hotmail.com>
2017-03-05 16:07:03 -0500

    Update md5kyruscrack1.py

--- md5kyruscrack1.py
+++ md5kyruscrack1.py
@@ -2,6 +2,7 @@ import hashlib
 
 
 answer = ""
+# Alphabet for testing each letter against puzzle
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", ".", "-", "?", "!", ":", ";", "(", ")", "[", "]", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "@", "#", "$", "%", "^", "&", "*", "+", "~", "`", "\\", "/", "<", ">", "{", "}", "_", "|", "\n"]
 
 cnt = 1
@@ -10,31 +11,27 @@ def hashmaker(word):
 	#print word
 	return hashlib.md5(word).hexdigest() + "\n"
 
+# Grabs line from puzzle
 with open("puzzle.txt","r") as f:
     for line in f:
     	print cnt
     	cnt+=1
-        #print line
-        #print "Testing"
+	# Compares generated hash for each alphabet letter to the hash in the puzzle
         for letter in alphabet:
-        	#print "Comparing... %r" % line 
+        	# print "Comparing... %r" % line 
         	if line == hashmaker(answer + letter):
         		#print "first"
-        		print "Y"
         		answer = answer + letter
         		break
+		# Compares uppercase letters
         	elif line == hashmaker(answer + letter.upper()):
-        		#print "second"
-        		print "Y"
+        		# print "second"
         		answer = answer + letter.upper()
         		break
-        	#else: #print "Failed."
-
-
 
 print "The answer is %r" % answer
 
-
+# Writes answer to text file
 print "Writing to part2.txt..."
 
 shafile = open("part2.txt", "w")
