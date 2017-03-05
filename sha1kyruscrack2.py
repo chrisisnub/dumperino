5947b4429ddcb48229d02269aa2a19a6d6806334
Chris Castro <cmc-castro@hotmail.com>
2017-03-05 15:54:28 -0500

    Update sha1kyruscrack2.py

--- sha1kyruscrack2.py
+++ sha1kyruscrack2.py
@@ -7,16 +7,12 @@ alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"
 cnt = 1
 
 def hashmaker(word):
-	#print word
-	#print hashlib.md5(word).hexdigest()
 	return hashlib.sha1(word).hexdigest() + "\n"
 
 with open("part2.txt","r") as f:
     for line in f:
     	print cnt
     	cnt+=1
-        #print line
-        #print "Testing"
         for letter in alphabet:
         	print "Comparing... %r" % line 
         	if line == hashmaker(answer + letter):
